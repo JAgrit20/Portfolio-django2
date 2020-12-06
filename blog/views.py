f176ce975c5b195d34171e7be78fc61f54d223c5
JAgrit20 <jagritacharya2020@gmail.com>
2020-12-06 14:45:40 +0530

    blogs page connected with DB

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def allblog(request):
-    return render(request,'blogs/allblogs.html') 
+    blogs = Blog.objects
+    return render(request,'blogs/allblogs.html',{'blogs':blogs}) 
