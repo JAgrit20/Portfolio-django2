ef0fb6e17c1826fccb957a7a35ae8243b0fc6a90
JAgrit20 <jagritacharya2020@gmail.com>
2020-12-06 21:05:33 +0530

    added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+try:
+    from local_settings import *
+except ImportError:
+    pass
